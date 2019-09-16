a818ea060d0cc0538b4d979f06ce2289bc6b96d2
sam laitha <samlaitha@sams-MacBook-Pro.local>
2019-09-15 23:06:19 -0700

    first commit

---
+++ flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by sam laitha on 9/11/19.
+//  Copyright © 2019 sam laitha. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
